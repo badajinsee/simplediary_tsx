1aac38cdc5be45350babe8a8d5e0e0696772971f
sunjin <changido2888@gmail.com>
2024-01-11 00:00:03 +0900

    [Add] useReducer 이용해서상태로직 분리하기 | dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable*/
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useReducer, useRef } from "react";
 
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
@@ -7,23 +7,69 @@ import DiaryList from "./DiaryList";
 
 import { Diary } from "./types";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "sunjin",
-//     content: "선진 공부중이다1.",
-//     emotion: 1,
-//     created_date: new Date().getTime(),
-//   }
-// ];
-
 interface ItType {
   email: string;
   body: string;
 }
 
+interface InitAction {
+  type: "INIT";
+  data: Diary[];
+}
+
+interface CreateAction {
+  type: "CREATE";
+  data: {
+    author: string;
+    content: string;
+    emotion: number;
+    id: number;
+  };
+}
+
+interface RemoveAction {
+  type: "REMOVE";
+  targetId: number;
+}
+
+interface EditAction {
+  type: "EDIT";
+  targetId: number;
+  newContent: string;
+}
+
+type Action = InitAction | CreateAction | RemoveAction | EditAction;
+
+const reducer = (state: Diary[], action: Action): Diary[] => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data, // 70번째줄 데이터
+        created_date,
+      };
+      return [newItem, ...state]; //원본 배열에 추가
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((it) =>
+        it.id === action.targetId ? { ...it, content: action.newContent } : it
+      );
+    }
+    default:
+      return state; //값이 안바뀌게 설정
+  }
+};
+
 function App() {
-  const [data, setData] = useState<Diary[]>([]);
+  // const [data, setData] = useState<Diary[]>([]);
+
+  const [data, dispatch] = useReducer(reducer, []);
 
   const dataId = useRef(0);
 
@@ -41,7 +87,7 @@ function App() {
           id: dataId.current++,
         };
       });
-      setData(initData);
+      dispatch({ type: "INIT", data: initData }); // type을 init으로 , 어떤 데이터 초기화 -> initdata
     } catch (error) {
       console.log(error);
     }
@@ -53,32 +99,43 @@ function App() {
 
   const onCreate = useCallback(
     (author: string, content: string, emotion: number) => {
-      const created_date = new Date().getTime();
-      const newItem = {
-        author,
-        content,
-        emotion,
-        created_date,
-        id: dataId.current,
-      };
-      dataId.current += 1;
-      setData((data) => [newItem, ...data]); //아이템을 추가한 데이터를 리턴
+      dispatch({
+        type: "CREATE",
+        data: { author, content, emotion, id: dataId.current++ },
+      });
+      // const created_date = new Date().getTime();
+      // const newItem = {
+      //   author,
+      //   content,
+      //   emotion,
+      //   created_date,
+      //   id: dataId.current,
+      // };
+      // dataId.current += 1;
+      // setData((data) => [newItem, ...data]); //아이템을 추가한 데이터를 리턴
     },
     [] //빈 배열로 전송하면 new 아이템 추가하면 리스트에 하나만 남게됨 , 함수형 업데이트 사용
   );
 
-  const onRemove = useCallback((targetId: number): void => {
+  const onRemove = useCallback((targetId: number) => {
+    dispatch({ type: "REMOVE", targetId });
+
     // const newDiaryList = data.filter((it) => it.id !== targetId);
-    setData((data) => data.filter((it) => it.id !== targetId));
+    // setData((data) => data.filter((it) => it.id !== targetId));
   }, []);
 
-  const onEdit = useCallback((targetId: number, newContent: string): void => {
-    setData((data) =>
-      data.map((it) =>
-        it.id === targetId ? { ...it, content: newContent } : it
-      )
-    );
-  }, []);
+  const onEdit = useCallback(
+    (targetId: number, newContent: string) => {
+      dispatch({ type: "EDIT", targetId, newContent });
+
+      // setData((data) =>
+      //   data.map((it) =>
+      //     it.id === targetId ? { ...it, content: newContent } : it
+      //   )
+      // );
+    },
+    [dispatch]
+  );
 
   // 감정 비율, usememo이용해서 리렌더링 줄이기
   const getDiaryAnalysis = useMemo(() => {
